3c78594375f2c9e3d9274694b04be88fad4c41bb
brian d foy <briandfoy@pobox.com>
2025-06-27 16:59:22 -0400

    initial creation

---
+++ lib/App/irr_rpsl_submit.pm
@@ -0,0 +1,65 @@
+package App::irr_rpsl_submit;
+use strict;
+
+use warnings;
+no warnings;
+
+our $VERSION = '0.001_01';
+
+=encoding utf8
+
+=head1 NAME
+
+App::irr_rpsl_submit - Perl program to submit RPSL to IRRdv4 (Irrdnet)
+
+=head1 SYNOPSIS
+
+	use App::irr_rpsl_submit;
+
+=head1 DESCRIPTION
+
+=over 4
+
+=item new
+
+=cut
+
+sub new {
+
+	}
+
+=item init
+
+=cut
+
+sub init {
+
+	}
+
+=back
+
+=head1 TO DO
+
+
+=head1 SEE ALSO
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+	http://github.com/briandfoy/app-irr_rpsl_submit
+
+=head1 AUTHOR
+
+brian d foy, C<< <briandfoy@pobox.com> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2025, brian d foy, All Rights Reserved.
+
+You may redistribute this under the terms of the Artistic License 2.0.
+
+=cut
+
+1;
